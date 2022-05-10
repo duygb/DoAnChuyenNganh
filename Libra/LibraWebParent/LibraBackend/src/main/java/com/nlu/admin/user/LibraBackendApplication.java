01fa1bff1293719e0a586bad8edacbbddcf0e384
thonghp <thonghoangpham123@gmail.com>
2022-05-10 10:01:32 +0700

    project initialization

---
+++ Libra/LibraWebParent/LibraBackend/src/main/java/com/nlu/admin/user/LibraBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nlu.admin.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraBackendApplication.class, args);
+    }
+
+}
---
+++ Libra/LibraWebParent/LibraFrontend/src/main/java/com/example/librafrontend/LibraFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.example.librafrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraFrontendApplication.class, args);
+    }
+
+}
