0fec1d83ca430c553d75ed35ac9584a3722cbc1e
thonghp <thonghoangpham123@gmail.com>
2022-06-05 21:16:10 +0700

    remove new and save function as it is no longer used

--- Libra/LibraWebParent/LibraBackend/src/main/java/com/nlu/admin/user/UserController.java
+++ Libra/LibraWebParent/LibraBackend/src/main/java/com/nlu/admin/user/UserController.java
@@ -1,6 +1,5 @@
 package com.nlu.admin.user;
 
-import com.nlu.common.entity.Role;
 import com.nlu.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -8,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -47,30 +45,6 @@ public class UserController {
     model.addAttribute("keyword", keyword);
 
     return "users";
-  }
-
-
-  @GetMapping("/users/new")
-  public String newUser(Model model) {
-    User user = new User();
-    List<Role> listRoles = userService.listRoles();
-
-    user.setEnabled(true);
-
-    model.addAttribute("user", user);
-    model.addAttribute("listRoles", listRoles);
-    model.addAttribute("pageTitle", "Tạo mới User");
-
-    return "user_form";
-  }
-
-  @PostMapping("/users/save")
-  public String saveUser(User user, RedirectAttributes redirectAttributes) {
-    userService.save(user);
-
-    redirectAttributes.addFlashAttribute("message", "User đã được lưu thành công !");
-
-    return "redirect:/users";
   }
 
   @GetMapping("/users/{id}/enabled/{status}")
